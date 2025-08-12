318603cf320eef08791f0b86ecdb2077e1a792e3
Piyush-shoundik <ritetopiyush@gmail.com>
2025-08-13 00:11:57 +0530

    code to take input from user and print it

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// first code to print c language 
+
+// #include <stdio.h>
+// int main(){
+//     printf("################################################################ \n");
+//     printf("\t\twelcome to my profile\n");
+//     printf("This is my first code writing in c programming language \n");
+//     printf("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// code to take input from user and print it 
+
 #include <stdio.h>
 int main(){
-    printf("################################################################ \n");
-    printf("\t\twelcome to my profile\n");
-    printf("This is my first code writing in c programming language \n");
-    printf("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+    int a , b;
+    printf("Print two number you want to print \n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d %d ", a , b);
+return(0);
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////
