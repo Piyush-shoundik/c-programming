7264903787128575a20c7f64bfa6b034688a946a
Piyush-shoundik <ritetopiyush@gmail.com>
2025-08-12 22:50:40 +0530

    firstcode

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    printf("################################################################ \n");
+    printf("\t\twelcome to my profile\n");
+    printf("This is my first code writing in c programming language \n");
+    printf("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+}
