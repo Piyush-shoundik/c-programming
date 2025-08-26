f1e9b094370392f7417abf247fc1694d6c2da4a6
Piyush-shoundik <ritetopiyush@gmail.com>
2025-08-26 23:52:57 +0530

    program to find largest among 3  operator.

--- main.c
+++ main.c
@@ -202,20 +202,79 @@
 // e. total=s2+p*(s-x)*(p+y)
 // f. print total
 
+// #include <stdio.h>
+
+// int main(){
+
+//     int x,y,p,s,total;
+
+//     printf("Enter your number for X ");
+//     scanf("%d", &x);
+//     printf("Enter your number for Y ");
+//     scanf("%d", &y);
+
+//     p = x * y;
+//     s = x + y;
+
+//     total = s * 2 + p* (s-x) * (p+y) ;
+//     printf("Your total is \t%d", total );
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// Given two numbers dividend and divisor, divide two numbers. The result of division should
+// truncate toward zero, which means losing its fractional part. For example, 8.345 would be
+// truncated to 8, and -2.7335 would be truncated to -2. Assume we are dealing with an environment
+// that could store dividend as signed long long integer and divisor as double.
+// Example
+// Input: dividend = 922337203685, divisor = 12.6
+// Output: 73201365371
+
+// #include <stdio.h>
+
+// int main() {
+//     long long dividend,result;
+//     double divisor;
+
+//     printf("Enter dividend: ");
+//     scanf("%lld", &dividend);
+
+//     printf("Enter divisor: ");
+//     scanf("%lf", &divisor);
+
+    
+//     result = (int)(dividend / divisor); 
+//     printf("Output: %lld\n", result);
+
+//     return 0;
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// Write a C program to find largest among 3 numbers using ternary (conditional) operator.
+
 #include <stdio.h>
+int main() {
+    int a, b, c;
 
-int main(){
+    printf("Enter your number for a: ");
+    scanf("%d", &a);
 
-    int x,y,p,s,total;
+    printf("Enter your number for b: ");
+    scanf("%d", &b);
 
-    printf("Enter your number for X ");
-    scanf("%d", &x);
-    printf("Enter your number for Y ");
-    scanf("%d", &y);
+    printf("Enter your number for c: ");
+    scanf("%d", &c);
 
-    p = x * y;
-    s = x + y;
+    if (a > b && a > c) {
+        printf("A is greater");
+    }
+    if (b > a && b > c) {
+        printf("B is greater");
+    }
+    if (c > a && c > b) {
+        printf("C is greater");
+    }
 
-    total = s * 2 + p* (s-x) * (p+y) ;
-    printf("Your total is \t%d", total );
+    return 0;
 }
