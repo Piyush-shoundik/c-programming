e04bc1df9ecf7c91f070e803397b17afc466cd95
Piyush-shoundik <ritetopiyush@gmail.com>
2025-09-03 23:38:37 +0530

    just_readme_section

--- main.c
+++ main.c
@@ -338,31 +338,37 @@
 
 // assigning grade
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    int marks;
+// int main() {
+//     int marks;
 
-    printf("Enter student marks (0-100): ");
-    scanf("%d", &marks);
+//     printf("Enter student marks (0-100): ");
+//     scanf("%d", &marks);
 
-    if (marks >= 90 && marks <= 100) {
-        printf("Grade: A+\n");
-    } else if (marks >= 80 && marks < 90) {
-        printf("Grade: A\n");
-    } else if (marks >= 70 && marks < 80) {
-        printf("Grade: A-\n");
-    } else if (marks >= 60 && marks < 70) {
-        printf("Grade: B\n");
-    } else if (marks >= 50 && marks < 60) {
-        printf("Grade: C\n");
-    } else if (marks >= 40 && marks < 50) {
-        printf("Grade: D\n");
-    } else if (marks >= 0 && marks < 40) {
-        printf("Grade: Fail\n");
-    } else {
-        printf("Invalid marks entered.\n");
-    }
+//     if (marks >= 90 && marks <= 100) {
+//         printf("Grade: A+\n");
+//     } else if (marks >= 80 && marks < 90) {
+//         printf("Grade: A\n");
+//     } else if (marks >= 70 && marks < 80) {
+//         printf("Grade: A-\n");
+//     } else if (marks >= 60 && marks < 70) {
+//         printf("Grade: B\n");
+//     } else if (marks >= 50 && marks < 60) {
+//         printf("Grade: C\n");
+//     } else if (marks >= 40 && marks < 50) {
+//         printf("Grade: D\n");
+//     } else if (marks >= 0 && marks < 40) {
+//         printf("Grade: Fail\n");
+//     } else {
+//         printf("Invalid marks entered.\n");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// so today i did't post any content because i was doing theory 
+
+//////////////////////////////////////////////////////////////////////////////////////////////
