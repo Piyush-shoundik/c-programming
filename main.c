ba6e1c5b6d0aede0f723d50da908d81ed4890e44
Piyush-shoundik <ritetopiyush@gmail.com>
2025-09-08 23:08:51 +0530

    check a given number is odd or even using bitwise operators

--- main.c
+++ main.c
@@ -372,3 +372,21 @@
 // so today i did't post any content because i was doing theory 
 
 //////////////////////////////////////////////////////////////////////////////////////////////
+
+// Write a C program to check a given number is odd or even using bitwise operators
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num | 1)
+        printf("%d is Odd\n", num);
+    else
+        printf("%d is Even\n", num);
+
+    return 0;
+}
